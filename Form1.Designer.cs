31f6af7ca5991593d6d7e872d745beae4791a9c1
AshikurRahmanShakib13 <168504068+AshikurRahmanShakib13@users.noreply.github.com>
2024-05-15 21:47:23 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            dataGridView1 = new System.Windows.Forms.DataGridView();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.ActiveCaption;
+            richTextBox1.Font = new Font("Sitka Subheading", 72F, FontStyle.Bold, GraphicsUnit.Point);
+            richTextBox1.Location = new Point(218, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(636, 161);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "DataGridView";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(812, 214);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(262, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(812, 307);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(262, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(812, 420);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(262, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(732, 211);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 26);
+            label1.TabIndex = 4;
+            label1.Text = "Name";
+            label1.Click += label1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(75, 192);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(600, 321);
+            dataGridView1.TabIndex = 5;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(720, 307);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 26);
+            label2.TabIndex = 6;
+            label2.Text = "UserID";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(693, 417);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 26);
+            label3.TabIndex = 7;
+            label3.Text = "Password";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(114, 544);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 62);
+            button1.TabIndex = 8;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(347, 544);
+            button2.Name = "button2";
+            button2.Size = new Size(157, 62);
+            button2.TabIndex = 9;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(585, 544);
+            button3.Name = "button3";
+            button3.Size = new Size(157, 62);
+            button3.TabIndex = 10;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Times New Roman", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(812, 544);
+            button4.Name = "button4";
+            button4.Size = new Size(157, 62);
+            button4.TabIndex = 11;
+            button4.Text = "Exit";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1131, 636);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
